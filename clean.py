94d73469f4a829f5c38b935abf9a0788d6904bb7
María González <120479987+mapagoh@users.noreply.github.com>
2023-04-13 14:47:21 -0500

    feat: code optimization

--- clean.py
+++ clean.py
@@ -4,18 +4,24 @@ import numpy as np
 
 data= pd.read_csv("data/data.csv")
 
-col = data.drop(['1990', '1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009'], axis=1)
-row = col.drop([0, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 17, 18, 19,20, 21, 22, 24, 26, 27, 28, 29, 30, 31, 32, 33, 35, 36, 37], axis=0)
+countries= ['Austria', 'Belgium', 'France', 'Ireland', 'Luxembourg', 'Netherlands', 'Switzerland']
 
-row.to_csv('data/data_clean.csv')
+df = pd.read_csv('data.csv', index_col=1)
+
+df= df.loc[df['country']. isin(countries)]
+
+df.to_csv('data/data_clean.csv')
 
 
 ## clean water data
 
 df= pd.read_csv("data/water.csv")
 
-df= df[['Country', '2010', '2015', '2016', '2017', '2018', '2019', '2020']]
-df= df.loc[df['Country']. isin(['Belgium', 'Denmark', 'Luxembourg', 'Netherlands', 'Switzerland'])]
+countries= ['Belgium', 'Denmark', 'Luxembourg', 'Netherlands', 'Switzerland']
+
+df = pd.read_csv('water.csv', index_col=1)
+
+df= df.loc[df['Country']. isin(countries)]
 
 df.to_csv('data/water_clean.csv')
 
@@ -38,5 +44,5 @@ def plot_country(country_name):
     df = pd.read_csv("electricity.csv")
     df = df[['Country', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020']]
     df = df.loc[df['Country'] == country_name]
-    
+
 df.to_csv('data/electricity_clean.csv')
--- visualization.py
+++ visualization.py
@@ -1,5 +1,3 @@
-from matplotlib import colors
-from matplotlib.colors import to_rgba
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,136 +5,55 @@ import matplotlib.pyplot as plt
 
 data= pd.read_csv("data/data_clean.csv")
 
-fig, ax= plt.subplots()
-year= ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020']
-emissions= [84150.23, 82007.46, 79309.94, 79772.02, 76235.15, 78486.74, 79468.30, 81792.16, 78558.03, 79740.74, 73592.02]
-bar_labels= ['b', 'g', 'b', 'c', 'r', 'g', 'r', 'b', 'r', 'g', 'b']
-bar_colors= ['tab:blue', 'tab:green', 'tab:blue', 'tab:cyan', 'tab:red', 'tab:green', 'tab:red', 'tab:blue', 'tab:red', 'tab:green', 'tab:blue']
-ax.bar(year, emissions, label=bar_labels, color=bar_colors)
-plt.title('Austria')
-fig.set_size_inches(10,5)
+def graph(title, emissions):
+      years = ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020']
+colors = {'2010': ('tab:blue', 'b'), '2011': ('tab:green', 'g'), '2012': ('tab:blue', 'b'), '2013': ('tab:cyan', 'c'),
+               '2014': ('tab:red', 'r'), '2015': ('tab:green', 'g'), '2016': ('tab:cyan', 'c'), '2017': ('tab:blue', 'b'), 
+                '2018': ('tab:green', 'g'), '2019': ('tab:blue', 'b'), '2020': ('tab:cyan', 'c')}
+
+fig, ax = plt.subplots()
+for i, years in enumerate(years):
+        ax.bar(years, emissions[i], label=colors[years][1], color=colors[years][0])
+plt.title(title)
+fig.set_size_inches(10, 5)
 plt.show()
 
-fig, ax= plt.subplots()
-year= ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020']
-emissions= [133645.77, 123133.69, 120361.60, 120458.32, 114768.44, 118955.47, 117417.88, 117097.88, 117594.04, 116448.19, 106433.26]
-bar_labels= ['b', 'g', 'b', 'c', 'r', 'g', 'r', 'b', 'r', 'g', 'b']
-bar_colors= ['tab:blue', 'tab:green', 'tab:blue', 'tab:cyan', 'tab:red', 'tab:green', 'tab:red', 'tab:blue', 'tab:red', 'tab:green', 'tab:blue']
-ax.bar(year, emissions, label=bar_labels, color=bar_colors)
-plt.title('Belgium')
-fig.set_size_inches(10,5)
-plt.show()
-
-fig, ax= plt.subplots()
-year= ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020']
-emissions= [512753.49, 488753.45, 490256.23, 491335.79, 461189.49, 464491.27, 466376.98, 469718.63, 450787.08, 441488.01, 399412.67]
-bar_labels= ['b', 'g', 'b', 'c', 'r', 'g', 'r', 'b', 'r', 'g', 'b']
-bar_colors= ['tab:blue', 'tab:green', 'tab:blue', 'tab:cyan', 'tab:red', 'tab:green', 'tab:red', 'tab:blue', 'tab:red', 'tab:green', 'tab:blue']
-ax.bar(year, emissions, label=bar_labels, color=bar_colors)
-plt.title('France')
-fig.set_size_inches(10,5)
-plt.show()
-
-fig, ax= plt.subplots()
-year= ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020']
-emissions= [61948.12, 57742.79, 58764.94, 58550.21, 58006.16, 60477.26, 62714.61, 62066.84, 62351.96, 59855.48, 57716.09]
-bar_labels= ['b', 'g', 'b', 'c', 'r', 'g', 'r', 'b', 'r', 'g', 'b']
-bar_colors= ['tab:blue', 'tab:green', 'tab:blue', 'tab:cyan', 'tab:red', 'tab:green', 'tab:red', 'tab:blue', 'tab:red', 'tab:green', 'tab:blue']
-ax.bar(year, emissions, label=bar_labels, color=bar_colors)
-plt.title('Ireland')
-fig.set_size_inches(10,5)
-plt.show()
-
-fig, ax= plt.subplots()
-year= ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020']
-emissions= [12168.53, 12052.28, 11809.92, 11276.75, 10797.43, 10317.72, 10078.73, 10261.75, 10561.47, 10732.70, 9064.90]
-bar_labels= ['b', 'g', 'b', 'c', 'r', 'g', 'r', 'b', 'r', 'g', 'b']
-bar_colors= ['tab:blue', 'tab:green', 'tab:blue', 'tab:cyan', 'tab:red', 'tab:green', 'tab:red', 'tab:blue', 'tab:red', 'tab:green', 'tab:blue']
-ax.bar(year, emissions, label=bar_labels, color=bar_colors)
-plt.title('Luxembourg')
-fig.set_size_inches(10,5)
-plt.show()
-
-fig, ax= plt.subplots()
-year= ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020']
-emissions= [211583.23, 197244.08, 193009.40, 193342.44, 185548.93, 192648.93, 193058.39, 190568.24, 185596.91, 179837.65, 163915.18]
-bar_labels= ['b', 'g', 'b', 'c', 'r', 'g', 'r', 'b', 'r', 'g', 'b']
-bar_colors= ['tab:blue', 'tab:green', 'tab:blue', 'tab:cyan', 'tab:red', 'tab:green', 'tab:red', 'tab:blue', 'tab:red', 'tab:green', 'tab:blue']
-ax.bar(year, emissions, label=bar_labels, color=bar_colors)
-plt.title('Netherlands')
-fig.set_size_inches(10,5)
-plt.show()
-
-fig, ax= plt.subplots()
-year= ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020']
-emissions= [54669.67, 50552.38, 51901.42, 52785.70, 48845.03, 48355.48, 48631.38, 47754.08, 46256.93, 45973.87, 43290.99]
-bar_labels= ['b', 'g', 'b', 'c', 'r', 'g', 'r', 'b', 'r', 'g', 'b']
-bar_colors= ['tab:blue', 'tab:green', 'tab:blue', 'tab:cyan', 'tab:red', 'tab:green', 'tab:red', 'tab:blue', 'tab:red', 'tab:green', 'tab:blue']
-ax.bar(year, emissions, label=bar_labels, color=bar_colors)
-plt.title('Switzerland')
-fig.set_size_inches(10,5)
-plt.show()
+graph('Austria', [84150.23, 82007.46, 79309.94, 79772.02, 76235.15, 78486.74, 79468.30, 81792.16, 78558.03, 79740.74, 73592.02])
+graph('Belgium', [133645.77, 123133.69, 120361.60, 120458.32, 114768.44, 118955.47, 117417.88, 117097.88, 117594.04, 116448.19, 106433.26])
+graph('France', [512753.49, 488753.45, 490256.23, 491335.79, 461189.49, 464491.27, 466376.98, 469718.63, 450787.08, 441488.01, 399412.67])
+graph('Ireland', [61948.12, 57742.79, 58764.94, 58550.21, 58006.16, 60477.26, 62714.61, 62066.84, 62351.96, 59855.48, 57716.09])
+graph('Luxembourg', [12168.53, 12052.28, 11809.92, 11276.75, 10797.43, 10317.72, 10078.73, 10261.75, 10561.47, 10732.70, 9064.90])
+graph('Netherlands', [211583.23, 197244.08, 193009.40, 193342.44, 185548.93, 192648.93, 193058.39, 190568.24, 185596.91, 179837.65, 163915.18])
+graph('Switzerland', [54669.67, 50552.38, 51901.42, 52785.70, 48845.03, 48355.48, 48631.38, 47754.08, 46256.93, 45973.87, 43290.99])
 
 ## visualization of water data
 
 df= pd.read_csv("data/water_clean.csv")
 
-fig, ax= plt.subplots()
-year= ['2010', '2015', '2016', '2017', '2018', '2019', '2020']
-public_water= [583.3, 568.2, 562.7, 565.8, 569.2, 570.9, 0]
-bar_labels= ['b', 'g', 'b', 'c', 'r', 'g', 'c']
-bar_colors= ['tab:blue', 'tab:green', 'tab:blue', 'tab:cyan', 'tab:red', 'tab:green', 'tab:cyan']
-ax.bar(year, public_water, label=bar_labels, color=bar_colors)
-plt.title('Belgium')
-fig.set_size_inches(10,5)
+def graph(title, public_water)
+years = ['2010', '2015', '2016', '2017', '2018', '2019', '2020']
+colors = {'2010': ('tab:blue', 'b'), '2015': ('tab:green', 'g'), '2016': ('tab:blue', 'b'), '2017': ('tab:cyan', 'c'),
+               '2018': ('tab:red', 'r'), '2019': ('tab:green', 'g'), '2020': ('tab:cyan', 'c')}
+
+fig, ax = plt.subplots()
+for i, years in enumerate(years):
+        ax.bar(years, public_water[i], label=colors[years][1], color=colors[years][0])
+plt.title(title)
+fig.set_size_inches(18, 5)
 plt.show()
 
-fig, ax= plt.subplots()
-year= ['2010', '2015', '2016', '2017', '2018', '2019', '2020']
-public_water= [363, 345.4, 351.8, 350.2, 361.2, 354, 365.9]
-bar_labels= ['b', 'g', 'b', 'c', 'r', 'g', 'c']
-bar_colors= ['tab:blue', 'tab:green', 'tab:blue', 'tab:cyan', 'tab:red', 'tab:green', 'tab:cyan']
-ax.bar(year, public_water, label=bar_labels, color=bar_colors)
-plt.title('Denmark')
-fig.set_size_inches(10,5)
-plt.show()
-
-fig, ax= plt.subplots()
-year= ['2010', '2015', '2016', '2017', '2018', '2019', '2020']
-public_water= [0, 42.2, 42.4, 43.2, 49.2, 43.7, 42.8]
-bar_labels= ['b', 'g', 'b', 'c', 'r', 'g', 'c']
-bar_colors= ['tab:blue', 'tab:green', 'tab:blue', 'tab:cyan', 'tab:red', 'tab:green', 'tab:cyan']
-ax.bar(year, public_water, label=bar_labels, color=bar_colors)
-plt.title('Luxembourg')
-fig.set_size_inches(10,5)
-plt.show()
-
-fig, ax= plt.subplots()
-year= ['2010', '2015', '2016', '2017', '2018', '2019', '2020']
-public_water= [1083.60, 1077.40, 1091.40, 1097.50, 1135.00, 1121.30, 0]
-bar_labels= ['b', 'g', 'b', 'c', 'r', 'g', 'c']
-bar_colors= ['tab:blue', 'tab:green', 'tab:blue', 'tab:cyan', 'tab:red', 'tab:green', 'tab:cyan']
-ax.bar(year, public_water, label=bar_labels, color=bar_colors)
-plt.title('Netherlands')
-fig.set_size_inches(10,5)
-plt.show()
-
-fig, ax= plt.subplots()
-year= ['2010', '2015', '2016', '2017', '2018', '2019', '2020']
-public_water= [816, 803, 804, 810, 829, 815, 824]
-bar_labels= ['b', 'g', 'b', 'c', 'r', 'g', 'c']
-bar_colors= ['tab:blue', 'tab:green', 'tab:blue', 'tab:cyan', 'tab:red', 'tab:green', 'tab:cyan']
-ax.bar(year, public_water, label=bar_labels, color=bar_colors)
-plt.title('Switzerland')
-fig.set_size_inches(10,5)
-plt.show()
+graph('Belgium', [583.3, 568.2, 562.7, 565.8, 569.2, 570.9, 0])
+graph('Denmark', [363, 345.4, 351.8, 350.2, 361.2, 354, 365.9])
+graph('Luxembourg', [0, 42.2, 42.4, 43.2, 49.2, 43.7, 42.8])
+graph('Netherlands', [1083.60, 1077.40, 1091.40, 1097.50, 1135.00, 1121.30, 0])
+graph('Switzerland', [816, 803, 804, 810, 829, 815, 824])
 
 ## visualization of biodiversity data
 
 df= pd.read_csv("data/biodiversity_clean.csv")
 
 def graph(title, percentage):
-      species = ['Mammals', 'Birds', 'Reptiles', 'Amphibians', 'Fish', 'Marine Fish', 'Freshwater Fish', 'Vascular plants', 'Mosses', 'Lichens', 'Invertebrates']
+    species = ['Mammals', 'Birds', 'Reptiles', 'Amphibians', 'Fish', 'Marine Fish', 'Freshwater Fish', 'Vascular plants', 'Mosses', 'Lichens', 'Invertebrates']
 colors = {'Mammals': ('tab:blue', 'b'), 'Birds': ('tab:green', 'g'), 'Reptiles': ('tab:blue', 'b'), 'Amphibians': ('tab:cyan', 'c'),
                'Fish': ('tab:red', 'r'), 'Marine Fish': ('tab:green', 'g'), 'Freshwater Fish': ('tab:cyan', 'c'), 'Vascular plants': ('tab:blue', 'b'),
                'Mosses': ('tab:green', 'g'), 'Lichens': ('tab:blue', 'b'), 'Invertebrates': ('tab:cyan', 'c')}
